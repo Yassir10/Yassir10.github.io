833840eaac91081ca064e4202115d12d38bc7a1c
Yassir10 <sellal.yassir@gmail.com>
2021-06-16 14:58:28 +0200

    page goes up after refreshing

--- src/js/app.js
+++ src/js/app.js
@@ -137,3 +137,11 @@ const dropMike = () => {
         }, 50)
     })
 }
+
+
+window.onload = function() {
+    window.setTimeout(
+        function() { window.scrollTo(0,0); },
+        10
+    );
+};
